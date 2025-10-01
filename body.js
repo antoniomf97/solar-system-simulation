f1eaec0fc0a240be5885304b2d1ab85ca14f94ff
antoniomf97 <antonio.mf97@gmail.com>
2025-10-01 11:36:03 +0100

    Updated with trails

--- body.js
+++ body.js
@@ -8,7 +8,9 @@ class Body {
         this.p = p5.Vector.fromAngle(this.a).mult(this.d);
         
         this.v = velocity;
-        this.color = color(c[0], c[1], c[2]);
+        this.color = c;
+        // this.color = color(c[0], c[1], c[2]);
+        this.trail = [];
     }
 
     get pos() {
@@ -18,6 +20,8 @@ class Body {
     update() {
         this.a += this.v * speedSlider.value();
         this.p = p5.Vector.fromAngle(this.a).mult(this.d);
+        this.trail.push(this.p);
+        this.trail = this.trail.slice(Math.max(this.trail.length - 100 + this.v*7, 0))
     }
 
     draw() {
@@ -25,6 +29,15 @@ class Body {
         translate(this.c.pos);
         fill(this.color);
         ellipse(this.p.x, this.p.y, this.r*2);
+
+        strokeWeight(1);
+        beginShape();
+        for (let p of this.trail) {
+            stroke(this.color[0], this.color[1], this.color[2], 100);
+            noFill();
+            vertex(p.x, p.y);
+        }
+        endShape();
         pop();
     }
 
@@ -32,7 +45,7 @@ class Body {
         push();
         noFill();
         strokeWeight(1);
-        stroke([50,50,50]);
+        stroke([40,40,40]);
         ellipse(this.c.pos.x, this.c.pos.y, this.d*2);
         pop();
     }
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function preload() {
 function setup() {
     createCanvas(1000, 600);
     
-    speedSlider = createSlider(0.0001, 0.1, 0.01, 0.0001);
+    speedSlider = createSlider(0.001, 0.1, 0.005, 0.001);
 
     sun = {
         r: 50,
@@ -22,21 +22,6 @@ function setup() {
         bodies.push(new Body(center=center, radius=body.radius, distance=body.distance, 
                     angle=body.angle, c=body.color, v=body.velocity));
     }
-
-    // bodys.push(new Body(center=sun, radius=10, distance=150, angle=90, c=[255,255,255]));
-    // bodys.push(new Body(center=bodys[0], radius=3, distance=25, angle=45, c=[255,255,255], tier=2));
-    // bodys.push(new Body(center=bodys[0], radius=2, distance=40, angle=20, c=[255,255,255], tier=3));
-    // bodys.push(new Body(center=bodys[0], radius=5, distance=50, angle=20, c=[255,255,255], tier=3));
-
-    // bodys.push(new Body(center=sun, radius=7, distance=75, angle=45, c=[255,255,255]));
-    // bodys.push(new Body(center=sun, radius=10, distance=220, angle=90, c=[255,255,255]));
-    // bodys.push(new Body(center=sun, radius=20, distance=270, angle=90, c=[255,255,255]));
-    // bodys.push(new Body(center=bodys[6], radius=3, distance=30, angle=90, c=[255,255,255]));
-    // bodys.push(new Body(center=sun, radius=30, distance=370, angle=45, c=[255,255,255]));
-    // bodys.push(new Body(center=bodys[8], radius=5, distance=60, angle=90, c=[255,255,255], tier=3));
-    
-    // bodys.push(new Body(center=bodys[9], radius=2, distance=10, angle=90, c=[255,255,255], tier=4));
-    // bodys.push(new Body(center=bodys[8], radius=5, distance=40, angle=90, c=[255,255,255], tier=3));
 }
 
 
@@ -48,7 +33,7 @@ function draw() {
 
     for (let body of bodies) {
         body.update();
-        body.orbit();
+        // body.orbit();
         body.draw();
     }
 }
